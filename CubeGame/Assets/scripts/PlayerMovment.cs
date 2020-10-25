351625f6be2319480b8065cb70429c1c8ecf0336
avivbuhbut <avivb931@gmail.com>
2020-10-26 00:10:57 +0200

    first commit 00:10 26.10.20

---
+++ CubeGame/Assets/scripts/PlayerMovment.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovment : MonoBehaviour
+{
+    public float speed;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        PlayerMovmentCotrol();
+    }
+
+
+    void PlayerMovmentCotrol()
+    {
+
+        if (Input.GetKeyDown("space"))
+        {
+           transform.Translate(Vector3.up * 8f * Time.deltaTime, Space.World);
+         }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed, Space.Self); //LEFT
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed, Space.Self); //RIGHT
+        }
+    }
+}
