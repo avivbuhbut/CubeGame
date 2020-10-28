e8dcff1c120cdc551e47f0813769b822305796e6
avivbuhbut <avivb931@gmail.com>
2020-10-28 23:21:59 +0200

    28.10.20 23:21

---
+++ CubeGame/Assets/Colided.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colided : MonoBehaviour
+{
+    public Animation anim;
+    public int counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    
+       // anim.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+   
+    public void OnTriggerEnter(Collider col)
+    {
+
+      
+        if (col.gameObject.tag == "PizzaBox")
+        {
+            counter++;
+            Debug.Log(counter);
+
+        }
+        
+
+
+
+
+            
+    }
+
+}
---
+++ CubeGame/Assets/movingFloor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingFloor : Colided
+{
+
+    private Animation anim;
+    [SerializeField] Animator animAtor ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //  anim = GameObject.Find("MovingDownCube").GetComponent<Animation>();
+        //  anim.enabled = true;
+        //anim = gameObject.GetComponent<Animation>();
+        //m.enabled = false;
+
+     
+      
+        animAtor.SetBool("Activate", false);
+    }
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (counter == 3)
+            animAtor.SetBool("Activate", true);
+
+    }
+}
