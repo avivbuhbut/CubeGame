83116014f66d8af59a61ed6f317e9e2437b27a75
avivbuhbut <avivb931@gmail.com>
2020-11-02 08:51:31 +0200

    02.11.20 08:51

--- CubeGame/Assets/ScoreCount.cs
+++ CubeGame/Assets/ScoreCount.cs
@@ -7,11 +7,11 @@ public class ScoreCount : MonoBehaviour
 {
 
 
-
+   
     public static int GlobalScore;
-    public int localScore;
+    public  int localScore;
     
-    float speed;
+   public float speed;
 
     /*Player2 Texts*/
     public Text GlobalScoreTextPlayer2;
@@ -23,17 +23,19 @@ public class ScoreCount : MonoBehaviour
     public Text LocalScorePlayer;
     public Text OutBoundsTextPlayer;
 
-    RaycastHit hit = new RaycastHit();
-    Vector3 vel;
+    public RaycastHit hit;
+    public Vector3 vel;
 
     public Rigidbody rigidbody;
     public Transform transform;
     // float pizzaSpeed = 20f;
-    float currentPosX = 0;
-    float currentPosY = 0;
-    float currentPosZ = 0;
+    public float currentPosX ;
+    public float currentPosY ;
+    public float currentPosZ ;
     void Start()
     {
+
+        hit = new RaycastHit();
         OutBoundsTextPlayer.enabled =false;
         LocalScorePlayer.enabled = false;
 
@@ -55,13 +57,18 @@ public class ScoreCount : MonoBehaviour
 
     void Update()
     {
-        GlobalScoreTextPlayer2.text = GlobalScoreTextPlayer.text;
-        LocalScorePlayer2.text = LocalScorePlayer.text;
-        OutBoundsTextPlayer2.text = OutBoundsTextPlayer.text;
+   
+       
+ 
 
-        LocalScorePlayer.enabled = false;
+    //   LocalScorePlayer.enabled = false;
+       // LocalScorePlayer2.enabled = false;
+
+
+
 
         GlobalScoreTextPlayer.text = "Total Score: " + GlobalScore;
+        GlobalScoreTextPlayer2.text = "Total Score: " + GlobalScore;
 
         vel = rigidbody.velocity;
 
@@ -78,22 +85,35 @@ public class ScoreCount : MonoBehaviour
         if ((vel.magnitude > 5) && (!Input.GetKey(KeyCode.Mouse0)))
         {
 
-
+       
             LocalScorePlayer.enabled = true;
+            LocalScorePlayer2.enabled = true;
 
             LocalScorePlayer.text = "Score: " + localScore;
+            LocalScorePlayer2.text = LocalScorePlayer.text;
+          
 
-            Debug.Log(localScore);
             localScore++;
 
             if (localScore < 100)
+            {
                 LocalScorePlayer.color = Color.red;
+                LocalScorePlayer2.color = Color.red;
+            }
             else if (localScore > 100)
             {
                 boxInAir();
                 LocalScorePlayer.color = Color.green;
+                LocalScorePlayer2.color = Color.green;
             }
-            
+
+            if (localScore == 0)
+            {
+                LocalScorePlayer.color = Color.red;
+                LocalScorePlayer2.color = Color.red;
+            }
+
+
 
         }
         else
@@ -104,8 +124,7 @@ public class ScoreCount : MonoBehaviour
         //player can touch each pizza box once !!! add code!
 
 
-             if (localScore == 0)
-            LocalScorePlayer.color = Color.red;
+       
 
 
 
@@ -128,11 +147,25 @@ public class ScoreCount : MonoBehaviour
         if (collision.gameObject.tag == "Bounds") //&& vel.magnitude> pizzaSpeed)
         {
 
-
+           
             StartCoroutine(ShowMessage(OutBoundsTextPlayer, "OUT OF BOUNDS!", 2));
+            StartCoroutine(ShowMessage(OutBoundsTextPlayer2, "OUT OF BOUNDS!", 2));
+            LocalScorePlayer.enabled = false;
+            LocalScorePlayer2.enabled = false;
             Debug.Log("hit left bound");
             gameObject.transform.position = new Vector3(currentPosX, currentPosY, currentPosZ);
         }
+
+
+        // if (collision.gameObject.tag == "Floor" || collision.gameObject.tag == "PizzaBox")
+        if (collision.gameObject.tag == "Floor" || collision.gameObject.tag == "PizzaBox" || collision.gameObject.tag == "Untagged")
+        {
+
+            LocalScorePlayer.enabled = false;
+            LocalScorePlayer2.enabled = false;
+        }
+
+
     }
 
 
