916477586449608163001b31ba79f7bfafc34eb5
avivbuhbut <avivb931@gmail.com>
2020-11-05 18:59:32 +0200

    05.11.20 18:59

---
+++ CubeGame/Assets/ColorChangingGrandually.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangeGrandually : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Color lerpedColor = Color.white;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+
+        lerpedColor = Color.Lerp(Color.white, Color.black, Time.time*2);
+        gameObject.GetComponent<MeshRenderer>().material.color = lerpedColor;
+    }
+}
+
+
