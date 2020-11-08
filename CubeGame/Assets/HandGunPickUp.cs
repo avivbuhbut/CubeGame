065478821057e5f3d80cc3cfae4738955554b5b7
avivbuhbut <avivb931@gmail.com>
2020-11-09 00:05:25 +0200

    09.10.20 00:05

---
+++ CubeGame/Assets/HandGunPickUp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandGunPickUp : MonoBehaviour
+{
+    public Transform WeaponHolderTrans;
+    public Transform HandGunTrans;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKey(KeyCode.Q))
+            transform.parent = null;
+
+    }
+
+
+
+     void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "HandGun")
+        {
+            HandGunTrans.transform.parent = WeaponHolderTrans.transform;
+            HandGunTrans.position = new Vector3(WeaponHolderTrans.position.x, WeaponHolderTrans.position.y, WeaponHolderTrans.position.z);
+
+        }
+    }
+}
