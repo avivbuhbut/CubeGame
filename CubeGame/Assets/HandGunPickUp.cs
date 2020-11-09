6c841799395884916f1bfc167931307565b8fcd1
avivbuhbut <avivb931@gmail.com>
2020-11-09 10:27:01 +0200

    09.11.20 10:26

--- CubeGame/Assets/HandGunPickUp.cs
+++ CubeGame/Assets/HandGunPickUp.cs
@@ -5,33 +5,133 @@ using UnityEngine;
 public class HandGunPickUp : MonoBehaviour
 {
     public Transform WeaponHolderTrans;
+    public Transform WeaponHolderTransLeftHand;
     public Transform HandGunTrans;
+
+
+    bool isFacingRight = true;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        HandGunTrans.GetComponent<Rigidbody>().isKinematic = true;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if(Input.GetKey(KeyCode.Q))
+       Debug.Log(Mathf.Atan2(WeaponHolderTrans.transform.right.z, WeaponHolderTrans.transform.right.x) * Mathf.Rad2Deg);
+       // Debug.Log("Player Pos: " + this.transform.position);
+
+        if (Input.GetKey(KeyCode.Q))
             transform.parent = null;
 
+
+        /*if the gun is in players hand , point to mouse pos*/
+        if (HandGunTrans.transform.parent == WeaponHolderTrans || HandGunTrans.transform.parent == WeaponHolderTransLeftHand)
+        {
+
+            /*RIGHT HAND*/
+           // if ((Mathf.Atan2(WeaponHolderTrans.transform.right.z, WeaponHolderTrans.transform.right.x) * Mathf.Rad2Deg) == 0) //  gun faces right
+           // {
+
+                HandGunTrans.transform.parent = WeaponHolderTrans;
+                HandGunTrans.transform.position = new Vector3(WeaponHolderTrans.position.x, WeaponHolderTrans.position.y, WeaponHolderTrans.position.z);
+                Debug.Log("Gun facing Right");
+                //rotation
+                Vector2 mousePos = Input.mousePosition;
+                // mousePos.z = 5.23f;
+
+                Vector2 objectPos = Camera.main.WorldToScreenPoint(HandGunTrans.position);
+                mousePos.x = mousePos.x - objectPos.x;
+                mousePos.y = mousePos.y - objectPos.y;
+
+                float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+                WeaponHolderTrans.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+
+
+
+
+
+          //  }
+           /* else //LEFT HAND
+            {
+
+                HandGunTrans.transform.parent = WeaponHolderTransLeftHand;
+                HandGunTrans.transform.position = new Vector3(WeaponHolderTransLeftHand.position.x, WeaponHolderTransLeftHand.position.y, WeaponHolderTransLeftHand.position.z);
+                //  FaceLeft();
+                Debug.Log("Gun facing Left");
+                //rotation
+                Vector2 mousePos = Input.mousePosition;
+                // mousePos.z = 5.23f;
+
+                Vector2 objectPos = Camera.main.WorldToScreenPoint(HandGunTrans.position);
+                mousePos.x = mousePos.x - objectPos.x;
+                mousePos.y = mousePos.y - objectPos.y;
+
+                float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+                WeaponHolderTransLeftHand.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+
+            }*/
+
+
+        }
+            
+            }
+
+
+
+
+        
+        
+
+    
+    
+
+
+
+
+
+    
+
+
+
+    void FaceRight()
+    {
+        if (!isFacingRight)
+        {
+            WeaponHolderTrans.transform.localScale = new Vector3(1, 1, 1);
+            isFacingRight = true;
+        }
     }
 
+    void FaceLeft()
+    {
+        if (isFacingRight)
+        {
+            WeaponHolderTrans.transform.localScale = new Vector3(-1, 1, 1);
+            isFacingRight = false;
+        }
+    }
 
-
-     void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "HandGun")
         {
-            HandGunTrans.transform.parent = WeaponHolderTrans.transform;
-            HandGunTrans.position = new Vector3(WeaponHolderTrans.position.x, WeaponHolderTrans.position.y, WeaponHolderTrans.position.z);
-
+          
+        
+            HandGunTrans.transform.parent = WeaponHolderTrans;
+            HandGunTrans.transform.position = new Vector3(WeaponHolderTrans.position.x, WeaponHolderTrans.position.y, WeaponHolderTrans.position.z);
+            // HandGunTrans.transform.localEulerAngles = new Vector3(0, 180f,180f);
+           //HandGunTrans.GetComponent<Rigidbody>().useGravity = false;
+            HandGunTrans.GetComponent<Rigidbody>().isKinematic =true;
+            //  HandGunTrans.position = new Vector3(WeaponHolderTrans.position.x, WeaponHolderTrans.position.y, WeaponHolderTrans.position.z);
+            //rotation
+          
         }
     }
 }
