a5d5283ab16e0107d466d1a5b5332ada93cc744e
avivbuhbut <avivb931@gmail.com>
2020-11-21 13:49:54 +0200

    21.11.20 13:49

---
+++ CubeGame/Assets/fillPizzaSauce.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fillPizzaSauce : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Vector3 PizzaSauce100pScale;
+    Vector3 fillSauce;
+
+    bool SuaceOnShelf;
+    void Start()
+    {
+        fillSauce = new Vector3(0.001f, 0.001f, 0.001f);
+        PizzaSauce100pScale = GameObject.Find("Sauce").transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+        if (SuaceOnShelf && GameObject.Find("Sauce").transform.localScale != PizzaSauce100pScale)
+            GameObject.Find("Sauce").transform.localScale += fillSauce;
+      
+        
+    }
+
+
+void OnCollisionEnter(Collision collision)
+{
+    if (collision.gameObject.transform.name == "Sauce")
+    {
+        SuaceOnShelf = true;
+    }
+
+        if (collision.gameObject.transform.name == "Floor")
+            SuaceOnShelf = false;
+}
+}
--- CubeGame/Assets/moveTowardsSuace.cs
+++ CubeGame/Assets/moveTowardsSuace.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class moveTowardsSuace : MonoBehaviour
 {
 
+    Vector3 scaleChangeBiger;
+    Vector3 scaleChangeSmaller;
+
     bool ColidedWithSouce;
     // Start is called before the first frame update
     void Start()
     {
-        
+        scaleChangeBiger = new Vector3(0.001f, 0.001f, 0.001f);
+        scaleChangeSmaller = new Vector3(-0.001f, -0.001f, -0.001f);
     }
 
     // Update is called once per frame
@@ -19,14 +23,26 @@ public class moveTowardsSuace : MonoBehaviour
         /*if the enemy didnt colided with the sauce move towards the sauce, else - move towards bounds*/
         if (!(ColidedWithSouce))
             transform.position = Vector3.MoveTowards(this.transform.position, GameObject.Find("Sauce").transform.position, .9f * Time.deltaTime); // move towards the pizza box
-       else
-            transform.position = Vector3.MoveTowards(this.transform.position, GameObject.Find("LeftBoundEnemy").transform.position, .9f * Time.deltaTime);
+      // else
+            //transform.position = Vector3.MoveTowards(this.transform.position, GameObject.Find("LeftBoundEnemy").transform.position, .9f * Time.deltaTime);
+
+
 
 
         /*if the enemy is now dead make the sauce go to the original sauce parent agian*/
        if(GameObject.Find("CubeEnemy").GetComponent<Enemy1Health>().health == 0)
         {
             GameObject.Find("Sauce").gameObject.transform.parent = GameObject.Find("PizzaIngridians").gameObject.transform;
+            ColidedWithSouce = false;
+        }
+
+
+        if (ColidedWithSouce)
+        {
+           // GameObject.Find("Sauce").transform.parent = null;
+            GameObject.Find("CubeEnemy").transform.localScale += scaleChangeBiger;
+            GameObject.Find("Sauce").transform.localScale += scaleChangeSmaller;
+
         }
 
     }
@@ -39,7 +55,7 @@ public class moveTowardsSuace : MonoBehaviour
       
             ColidedWithSouce = true;
 
-            GameObject.Find("Sauce").gameObject.transform.parent = GameObject.Find("CubeEnemy").transform;
+           // GameObject.Find("Sauce").gameObject.transform.parent = GameObject.Find("CubeEnemy").transform;
         }
     }
 }
