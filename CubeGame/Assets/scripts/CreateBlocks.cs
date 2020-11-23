6ed64a3154103770de23d44db61fa0a5bbf2752c
avivbuhbut <avivb931@gmail.com>
2020-11-23 20:48:45 +0200

    23.11.20 20:48

---
+++ CubeGame/Assets/scripts/CreateBlocks.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBlocks : MonoBehaviour
+{
+
+    public static int BlocksAvilable;
+  //  public GameObject CubeGamObj;
+    public GameObject CubeGamObj;
+    private GameObject linehandler;
+    private Vector3 mousepos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CubeGamObj.SetActive(false);
+    }
+
+   
+
+    void Update()
+    {
+
+
+        if(Input.GetKey(KeyCode.B)) 
+        if (BlocksAvilable > 0 )
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+
+                BlocksAvilable--; // every time createing a block
+                   GameObject.Find("Player").transform.GetComponent<BlocksAvilableTMPControl>().PlayerBlocksAvilableTMP.text = "X" + CreateBlocks.BlocksAvilable + " Blocks";
+                    CubeGamObj.SetActive(true);
+                mousepos = Input.mousePosition;
+                mousepos.z = 14.5f;
+
+                mousepos = Camera.main.ScreenToWorldPoint(mousepos);
+                linehandler = Instantiate(CubeGamObj, mousepos, Quaternion.identity) as GameObject;
+            }
+        }
+
+        /*delete a block*/
+        if (Input.GetKey(KeyCode.H))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+              // every time createing a block
+
+
+                 	Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);                    ///Very Useful Code! - tells the object name when clicked by the mouse
+                	RaycastHit hit;
+            	// Casts the ray and get the first game object hit
+                	Physics.Raycast(ray, out hit);
+   	                Debug.Log("This hit at " + hit.transform.name ); // till here
+
+                if (hit.transform.name == "CubePlayerCreate(Clone)")
+                {
+
+                    BlocksAvilable++;
+                    hit.transform.gameObject.SetActive(false);
+                    GameObject.Find("Player").transform.GetComponent<BlocksAvilableTMPControl>().PlayerBlocksAvilableTMP.enabled = true;
+                GameObject.Find("Player").transform.GetComponent<BlocksAvilableTMPControl>(). PlayerBlocksAvilableTMP.text = "X" + CreateBlocks.BlocksAvilable + " Blocks";
+                }
+            }
+        }
+
+        //CubeGamObj.GetComponent<Rigidbody>().isKinematic = false;
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //if(collision.gameObject.transform.tag == "Floor")
+         //   Instantiate(CubeGamObj, new Vector3(this.transform.position.x, 0, 0), Quaternion.identity);
+    }
+}
---
+++ CubeGame/Assets/scripts/moveTowardsSuace.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveTowardsSuace : MonoBehaviour
+{
+
+    Vector3 scaleChangeBiger;
+    Vector3 scaleChangeSmaller;
+
+    Vector3 StartPosEnemy;
+    public Transform SauceTrans;
+    bool ColidedWithSouce;
+
+    int NewEnemyCounter = 0;
+    Transform NewEnemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scaleChangeBiger = new Vector3(0.001f, 0.001f, 0.001f);
+        scaleChangeSmaller = new Vector3(-0.001f, -0.001f, -0.001f);
+        StartPosEnemy = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        /*if the enemy didnt colided with the sauce move towards the sauce, else - move towards bounds*/
+        if (!(ColidedWithSouce) && transform.name != "NewEnemy")
+            transform.position = Vector3.MoveTowards(this.transform.position, GameObject.Find("Sauce").transform.position, .9f * Time.deltaTime); // move towards the pizza box
+        else
+            if(NewEnemy!= null)
+            NewEnemy.position = Vector3.MoveTowards(NewEnemy.position, GameObject.Find("Player").transform.position, .9f * Time.deltaTime);
+
+
+        /*if the enemy is now dead make the sauce go to the original sauce parent agian*/
+        if (GameObject.Find("CubeEnemy").GetComponent<Enemy1Health>().health == 0)
+        {
+            GameObject.Find("Sauce").gameObject.transform.parent = GameObject.Find("PizzaIngridians").gameObject.transform;
+            ColidedWithSouce = false;
+        }
+
+
+
+       /*if the sauce scale is bigger than 1 keep decresing it  - else turn of the colision (to stop the decresing of the sauce and icresing of the enemy)*/
+        if (ColidedWithSouce && SauceTrans.localScale.x > 0)
+        {
+
+    
+            GameObject.Find("CubeEnemy").transform.localScale += scaleChangeBiger;
+            GameObject.Find("Sauce").transform.localScale += scaleChangeSmaller;
+
+        }
+        else
+            ColidedWithSouce = false;
+
+
+
+        if(ColidedWithSouce && NewEnemyCounter<1)
+        {
+            NewEnemyCounter++;
+             NewEnemy = Instantiate(GameObject.Find("CubeEnemy").transform, StartPosEnemy, Quaternion.identity);
+            NewEnemy.name = "NewEnemy";
+            NewEnemy.position = Vector3.MoveTowards(NewEnemy.position, GameObject.Find("Player").transform.position, .9f * Time.deltaTime);
+
+            
+        }
+
+
+    }
+
+
+     void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.transform.tag== "Sauce")
+        {
+            Debug.Log(" SauceTrans.localScale.x : " + SauceTrans.localScale.x);
+            ColidedWithSouce = true;
+
+           // GameObject.Find("Sauce").gameObject.transform.parent = GameObject.Find("CubeEnemy").transform;
+        }else
+            ColidedWithSouce = false;
+    }
+}
