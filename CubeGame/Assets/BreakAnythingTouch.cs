e8e2415292dfd34c220fa104f07f85ce86044c11
avivbuhbut <avivb931@gmail.com>
2020-12-07 23:45:00 +0200

    07.12.20 23:44

---
+++ CubeGame/Assets/BreakAnythingTouch.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakAnythingTouch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    int counter = 0;
+
+     void OnCollisionEnter(Collision collision)
+    {
+        if (counter < 10)
+        {
+            if (collision.transform.tag != "Player" && collision.transform.name != "Broken") { 
+            collision.transform.GetComponent<MeshDestroy>().DestroyMesh(collision.transform.gameObject.GetComponent<MeshFilter>().mesh, collision.transform.GetComponent<MeshDestroy>(), collision.gameObject);
+            counter++;
+        }
+        }
+        //try to make it break 5 times! 
+
+
+
+
+      //  if(collision.transform.name == "Broken" && counter<10)
+      //  {
+         //   collision.transform.GetComponent<MeshDestroy>().DestroyMesh(collision.transform.gameObject.GetComponent<MeshFilter>().mesh, collision.transform.GetComponent<MeshDestroy>(), collision.gameObject);
+
+        //    counter++;
+     //   }
+
+      
+    }
+    
+    void OnCollisionExit(Collision collision)
+    {
+        counter = 0;
+    }
+}
