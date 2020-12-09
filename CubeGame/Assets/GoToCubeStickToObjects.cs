a80125d022c5e8c109e6cdbfab42757c0c0226f2
avivbuhbut <avivb931@gmail.com>
2020-12-09 22:29:34 +0200

    09.12.20 22:29

---
+++ CubeGame/Assets/GoToCubeStickToObjects.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToCubeStickToObjects : MonoBehaviour
+{
+
+    RaycastHit hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);                    ///Very Useful Code! - tells the object name when clicked by the mouse
+
+        // Casts the ray and get the first game object hit
+        Physics.Raycast(ray, out hit);
+
+        if (hit.transform.name == "GoToStickCube")
+            this.transform.GetComponent<Rigidbody>().isKinematic = false;
+
+    }
+
+     void OnCollisionEnter(Collision collision)
+    {
+
+        if(collision.transform.name == "CubeDistraction (3)")
+        this.transform.GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
