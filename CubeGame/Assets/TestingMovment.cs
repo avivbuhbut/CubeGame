60bf0de31fcfd31d53cd4cc1a8a773324238a2d0
avivbuhbut <avivb931@gmail.com>
2021-05-12 21:51:48 +0300

    12.05.21 21:51

---
+++ CubeGame/Assets/TestingMovment.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingMovment : MonoBehaviour
+{
+
+    public Animator animatorWalking;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            animatorWalking.SetBool("Activate", true);
+        }
+    }
+}
