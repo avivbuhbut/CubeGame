ce1e6d74a900d919dbb66f49f301861ef6ffe7b0
avivbuhbut <avivb931@gmail.com>
2021-06-02 22:53:56 +0300

    23:09 02.06.21

--- CubeGame/Assets/scripts/Fire.cs
+++ CubeGame/Assets/scripts/Fire.cs
@@ -32,7 +32,7 @@ public class Fire : MonoBehaviour
     {
 
 
-        if (Input.GetMouseButtonDown(0) &&( this.gameObject.transform.parent == WeaponHolder.transform) || Input.GetMouseButtonDown(0) && (this.gameObject.transform.parent == WeaponHolderLeftHand.transform))
+        if (Input.GetMouseButtonDown(0) )
         {
             /*play particals when shooting*/
             ShellParticles.Play();
@@ -44,7 +44,7 @@ public class Fire : MonoBehaviour
            if ( Physics.Raycast(AimTransform.transform.position, AimTransform.transform.right, out hit))
             {
 
-
+                Debug.Log(hit.transform.name);
 
                 EnamiesTakesDamge();
 
